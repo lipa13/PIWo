12fec87a9658a7ce5d43e906319cc339aee73b94
lipa13 <jaklip04@gmail.com>
2025-04-27 03:49:27 +0200

    react basic project structure

---
+++ lab3/app/root.jsx
@@ -0,0 +1,8 @@
+import {
+  isRouteErrorResponse,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "react-router";
---
+++ lab3/app/routes.js
@@ -0,0 +1,3 @@
+import { index } from "@react-router/dev/routes";
+
+export default [index("pages/home/home.jsx")];
