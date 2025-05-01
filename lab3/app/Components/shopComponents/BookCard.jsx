988f876bcbbb10b33a58ad1728888dde60b9195b
lipa13 <jaklip04@gmail.com>
2025-05-01 02:32:32 +0200

    added like button

--- lab3/app/Components/shopComponents/BookCard.jsx
+++ lab3/app/Components/shopComponents/BookCard.jsx
@@ -1,7 +1,15 @@
+import HeartIcon from "../../../assets/heart-outline.svg?react";
+
 export default function BookCard(){
     return (
         <div className="bookcard">
-            <div className="bookcard-img">Photos</div>
+            <div className="bookcard-img">
+                Photos
+                <div className="like-btn-container">
+                    <button className="like-btn"><HeartIcon className="heart-icon"/></button>
+                    <p className="like-counter">0</p>
+                </div>
+            </div>
             <div className="bookcard-upload-time">Uploaded 1 day(s) ago</div>
             <div className="bookcard-info">
                 <p className="bookcard-title">Very Interesting Book</p>
