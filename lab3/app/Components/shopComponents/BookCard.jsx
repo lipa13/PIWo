a5f42b26503edbdc40a3e876aa26aa7751002268
lipa13 <jaklip04@gmail.com>
2025-05-01 02:02:44 +0200

    more and more styling

--- lab3/app/Components/shopComponents/BookCard.jsx
+++ lab3/app/Components/shopComponents/BookCard.jsx
@@ -2,8 +2,12 @@ export default function BookCard(){
     return (
         <div className="bookcard">
             <div className="bookcard-img">Photos</div>
-
-
+            <div className="bookcard-upload-time">Uploaded 1 day(s) ago</div>
+            <div className="bookcard-info">
+                <p className="bookcard-title">Very Interesting Book</p>
+                <p className="bookcard-price">10.99$</p>
+                <button className="btn bookcard-btn"></button>
+            </div>
         </div>
     )
 }
