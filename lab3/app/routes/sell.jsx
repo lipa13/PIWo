e131254feaf48d0c65f9481d9f8561e4c2d751fb
lipa13 <jaklip04@gmail.com>
2025-05-02 03:47:42 +0200

    base of the sell view

--- lab3/app/routes/sell.jsx
+++ lab3/app/routes/sell.jsx
@@ -1,13 +1,53 @@
+import "../styles/sellStyle.css";
+
 export default function Sell(){
     return (
-        <form className="sell-form">
-            <label className="Name">Name:</label>
-            <input type="text"/>
+        <div className="sell-container">
+            <h3>Sell your book now with this simple form!</h3>
+            <form className="sell-form">
+                <div className="sell-div">
+                    <label className="sell-title">Title:</label>
+                    <input className="input-sell input-sell-title" type="text"/>
+                </div>
 
-            <label className="description">Description:</label>
-            <input type="text"/>
+                <div className="sell-div">
+                    <label className="sell-author">Author:</label>
+                    <input className="input-sell input-sell-author" type="text"/>
+                </div>
 
-            <button className="btn sell">Submit</button>
-        </form>
+                <div className="sell-div">
+                    <label className="sell-category">Category:</label>
+                </div>
+
+                <div className="sell-div">
+                    <label className="sell-description">Description:</label>
+                    <textarea
+                        className="input-sell input-sell-description"
+                        id="multi-input"
+                        rows={4}
+                    />
+                </div>
+
+                <div className="sell-div">
+                    <label className="sell-condition">Condition:</label>
+                </div>
+
+                <div className="sell-div">
+                    <label className="sell-pages">Pages:</label>
+                    <input className="input-sell input-sell-pages" type="number"/>
+                </div>
+
+                <div className="sell-div">
+                    <label className="sell-cover">Cover:</label>
+                </div>
+
+                <div className="sell-div">
+                    <label className="sell-price">Price:</label>
+                    <input className="input-sell input-sell-price" type="number"/>
+                </div>
+
+                <button className="btn sell">Submit</button>
+            </form>
+        </div>
     );
 }
