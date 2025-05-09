438beb03f6dda8efb94d6aa5976bb54530b8ee67
lipa13 <jaklip04@gmail.com>
2025-05-09 03:17:12 +0200

    further adjustments in fliters design

--- lab3/app/Components/shopComponents/FilterList.jsx
+++ lab3/app/Components/shopComponents/FilterList.jsx
@@ -2,9 +2,48 @@
 export default function FilterList() {
     return (
         <div className="filter-list-container">
-            <div className="filter-list">
-
-            </div>
+            <ul className="filter-list">
+                <li className="filter-list-item">
+                    <button className="filter-list-item-btn">
+                        <p className="filter-list-item-label">
+                            Author 1
+                        </p>
+                        <input type="checkbox" />
+                    </button>
+                </li>
+                <li className="filter-list-item">
+                    <button className="filter-list-item-btn">
+                        <p className="filter-list-item-label">
+                            Author 2
+                        </p>
+                        <input type="checkbox" />
+                    </button>
+                </li>
+                <li className="filter-list-item">
+                    <button className="filter-list-item-btn">
+                        <p className="filter-list-item-label">
+                            Author 3
+                        </p>
+                        <input type="checkbox" />
+                    </button>
+                </li>
+                <li className="filter-list-item">
+                    <button className="filter-list-item-btn">
+                        <p className="filter-list-item-label">
+                            Author 4
+                        </p>
+                        <input type="checkbox" />
+                    </button>
+                </li>
+                <li className="filter-list-item">
+                    <button className="filter-list-item-btn">
+                        <p className="filter-list-item-label">
+                            Author 5
+                        </p>
+                        <input type="checkbox" />
+                    </button>
+                </li>
+            </ul>
         </div>
     )
 }
