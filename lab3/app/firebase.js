9e5c809e7b47bf2482dcad31f90900630df21154
lipa13 <jaklip04@gmail.com>
2025-06-08 21:56:59 +0200

    fix

--- lab3/app/firebase.js
+++ lab3/app/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA1i5ncUQr33R8MHA_o7drt3N1XXMyulbE",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: "piwo-labs-2025.firebaseapp.com",
     projectId: "piwo-labs-2025",
     storageBucket: "piwo-labs-2025.firebasestorage.app",
     messagingSenderId: "933563237080",
-    appId: "1:933563237080:web:4bac6851e80601f1280dad"
+    appId: "1:933563237080:web:4390eb66aaf0e585280dad"
 };
 
 const app = initializeApp(firebaseConfig);
